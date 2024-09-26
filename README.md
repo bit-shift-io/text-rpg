0c9ecb612d87c2585f5152a0839d7de0408a6042
Fabian Mathews <fabian@jpmedia.com.au>
2024-09-26 16:38:01 +0930

    switched to use aichat, as used by chaz

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Text RPG
 
+Install AIChat: https://github.com/sigoden/aichat
 
+brew install aichat
 
 Kalosm - Rust LLM framework - https://floneum.com/kalosm/docs/
 Headjack - Rust Matrix wrapped - https://docs.rs/headjack/0.4.0/headjack/index.html
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{fmt, fs::{self, File}};
+use std::{collections::HashMap, io::Read, path::PathBuf, sync::Mutex};
 
 use headjack::*;
-use kalosm::{language::{Chat, Llama}, sound::TextStream};
+//use kalosm::{language::{Chat, Llama}, sound::TextStream};
 use matrix_sdk::{
     media::{MediaFileHandle, MediaFormat, MediaRequest},
     room::MessagesOptions,
@@ -13,6 +14,10 @@ use matrix_sdk::{
 };
 use serde::Deserialize;
 use tracing::{error, info};
+use lazy_static::lazy_static;
+
+mod aichat;
+use aichat::AiChat;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
@@ -26,14 +31,28 @@ impl fmt::Display for Config {
     }
 }
 
+lazy_static! {
+    /// Holds the config for the bot
+    static ref GLOBAL_CONFIG: Mutex<Option<Config>> = Mutex::new(None);
+
+    /// Count of the global messages per user
+    static ref GLOBAL_MESSAGES: Mutex<HashMap<String, u64>> = Mutex::new(HashMap::new());
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> { //anyhow::Error> {
     
     let mut file_contents = fs::read_to_string("config.yml").expect("Unable to read config.yml");
     let config: Config = serde_yml::from_str(&file_contents).unwrap();
+    *GLOBAL_CONFIG.lock().unwrap() = Some(config.clone());
 
     //println!("config: {}", config);
 
+    /// Returns the backend based on the global config
+    fn get_backend() -> AiChat {
+        let config = GLOBAL_CONFIG.lock().unwrap().clone().unwrap();
+        AiChat::new("aichat".to_string(), None) //Some("/Users/fabian/Library/Application Support/aichat/".to_owned())) //config.aichat_config_dir.clone())
+    }
 
     // see example usage here on how to load from config: https://github.com/arcuru/chaz/blob/main/src/main.rs
     let bot_config = BotConfig {
@@ -68,13 +87,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> { //anyhow::Error> {
 
     info!("The client is ready! Listening to new messages…");
 
-
+/* 
     let model = Llama::new_chat().await.unwrap();
 
     // construct LLM chat model
     let mut chat = Chat::builder(model)
         .with_system_prompt("The assistant will act like a pirate")
         .build();
+*/
 
     // The party command is from the matrix-rust-sdk examples
     // Keeping it as an easter egg
@@ -96,8 +116,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> { //anyhow::Error> {
         "ask",
         "".to_string(),
         "Ask a question".to_string(),
-        |_sender, body, room| async move {
+        |sender, body, room| async move {
 
+            // Skip over the command, which is "!chaz ask"
+            let input = body
+                .split_whitespace()
+                .skip(1)
+                .collect::<Vec<&str>>()
+                .join(" ");
+            /* *
             let model = Llama::new_chat().await.unwrap();
 
             // construct LLM chat model
@@ -107,9 +134,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> { //anyhow::Error> {
 
             //let response = format!("You asked: {}", body); 
             let response = chat.add_message(body).all_text().await;
+            */
 
+
+            //let model = Some("gemini".to_owned()); // todo: move to config
+
+            info!(
+                "Request: {} - {}",
+                sender.as_str(),
+                input.replace('\n', " ")
+            );
+            if let Ok(result) = get_backend().execute(&None, input.to_string(), Vec::new()) {
+                // Add the prefix ".response:\n" to the result
+                // That way we can identify our own responses and ignore them for context
+                info!(
+                    "Response: {} - {}",
+                    sender.as_str(),
+                    result.replace('\n', " ")
+                );
+                let content = RoomMessageEventContent::notice_plain(result);
+
+                room.send(content).await.unwrap();
+            }
+            /* 
             let content = RoomMessageEventContent::notice_plain(response);
             room.send(content).await.unwrap();
+            */
             Ok(())
         },
     )
