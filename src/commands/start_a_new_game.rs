93bc0e600026f0d3b226f57c6836e80234f10fed
Fabian Mathews <fabian@jpmedia.com.au>
2024-09-29 09:47:57 +0930

    working on describing players

--- src/commands/start_a_new_game.rs
+++ src/commands/start_a_new_game.rs
@@ -60,60 +60,69 @@ fn extract_json_from_response(hay: &str) -> Vec<&str> {
 
 
 
+const RAW_PROMPT = r#"
+I am a dungeon master. I need to create a setup for the game.
+
+I need a list of each room. 
+    In each room I need a list of monsters. 
+    In each room I need a list of items.
+I need a list of room connections.
+I need a list of monsters with any special abilities they might have.
+I need a list of player characters that describe the character for each player who is playing.
+There are ${num_players} players.
+
+Each room should be described with the following structure:
+
+    Room number: The room number.
+    Name: The name of the room.
+    Description: The description of the room.
+    Monsters: A comma separated list of monsters in the room.
+    Items: A comma separated list of items in the room.
+
+Each room connection should be described with the following structure:
+
+    Room Connections: A list of connected room numbers.
+
+Each monstor should be described with the following structure:
+
+    Monstor name: The name of a monster.
+    Description: A short description of the monster.
+    Abilities: A comma separated list of special abilties the monster has.
+
+
+I need the response in JSON format. "rooms" should be an array with the following structure:
+{
+    "room_number": {The room number},
+    "name": {The name of the room},
+    "description": {The description of the room},
+    "monsters": [A comma separated list of monsters in the room],
+    "items": [A comma separated list of items in the room]
+}
+
+"monsters" should be an array with the following structure:
+{
+    "name": {The name of a monster},
+    "description": {A short description of the monster},
+    "abilities": [A comma separated list of special abilties the monster has],
+}
+
+"room_connections" should be an arry with the following structure:
+{
+    "room_number": 1,
+    "connected_rooms": [A list of connected room numbers]
+}
+
+"player_characters" should be an arry with the following structure:
+{
+    "character_type": {The type of character, eg. Barbarian}
+}
+"#;
 
 pub async fn start_a_new_game(sender: OwnedUserId, text: String, room: MatrixRoom) -> Result<(), ()> {
     room.send(RoomMessageEventContent::notice_plain("Let me go grab the game and set it up...")).await.unwrap();
 
-    let prompt = r#"
-        I am a dungeon master. I need to create a setup for the game.
-
-        I need a list of each room. 
-            In each room I need a list of monsters. 
-            In each room I need a list of items.
-        I need a list of room connections.
-        I need a list of monsters with any special abilities they might have.
-
-        Each room should be described with the following structure:
-
-            Room number: The room number.
-            Name: The name of the room.
-            Description: The description of the room.
-            Monsters: A comma separated list of monsters in the room.
-            Items: A comma separated list of items in the room.
-
-        Each room connection should be described with the following structure:
-
-            Room Connections: A list of connected room numbers.
-
-        Each monstor should be described with the following structure:
-
-            Monstor name: The name of a monster.
-            Description: A short description of the monster.
-            Abilities: A comma separated list of special abilties the monster has.
-
-
-        I need the response in JSON format. "rooms" should be an array with the following structure:
-        {
-            "room_number": {The room number},
-            "name": {The name of the room},
-            "description": {The description of the room},
-            "monsters": [A comma separated list of monsters in the room],
-            "items": [A comma separated list of items in the room]
-        }
-
-        "monsters" should be an array with the following structure:
-        {
-            "name": {The name of a monster},
-            "description": {A short description of the monster},
-            "abilities": [A comma separated list of special abilties the monster has],
-        }
-
-        "room_connections" should be an arry with the following structure:
-        {
-            "room_number": 1,
-            "connected_rooms": [A list of connected room numbers]
-        }
-    "#;
+    let num_players = 2; // todo: fix me
+    let prompt = RAW_PROMPT.replace("${num_players}", num_players);
 
     if let Ok(result) = get_ai_chat().execute(&None, prompt.to_string(), Vec::new()) {
         let json_strs = extract_json_from_response(&result);
@@ -171,6 +180,8 @@ pub async fn start_a_new_game(sender: OwnedUserId, text: String, room: MatrixRoo
                 }
     
                 room.send(RoomMessageEventContent::notice_plain("Okay, a new game is ready. Let's begin.")).await.unwrap();
+
+                // todo: describe the starting room, the quest and each player as a story
             },
             Err(err) => {
                 error!("Error parsing json: {err}");
