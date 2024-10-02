63c7bdeea438d4a27a2a3b35d19dfb3dd8a6d72a
Fabian Mathews <fabian@jpmedia.com.au>
2024-10-02 15:05:01 +0930

    act now works

--- src/commands/act.rs
+++ src/commands/act.rs
@@ -22,7 +22,9 @@ I am a dungeon master. A player has asked me to make an action.
 I need to take the current state of the game and update it to reflect this users action.
 
 The current state of the game is:
+```json
 ${game_state}
+```
 
 The player has asked me to:
 ${action}
@@ -31,6 +33,26 @@ Please respond with the updated state of the game.
 The format must be the same as the current game state.
 I need the response in JSON format.
 Do not prompt for further instructions, if you are unsure make your best guess.
+"#;
+
+const ACT_STORY_RAW_PROMPT: &str = r#"
+I am a dungeon master. A player has asked me to take an action.
+I have modified the game state in accordance with the players action but I need you to describe for me the action as a story.
+
+The previous state of the game was:
+```json
+${previous_game_state}
+```
+
+The new state of the game after the players action is:
+```json
+${new_game_state}
+```
+
+The player has asked me to:
+${action}
+
+Please include in your response exact values for things such as damage.
 "#;
 
 pub async fn act(sender: OwnedUserId, text: String, room: MatrixRoom) -> Result<(), ()> {
@@ -57,7 +79,7 @@ pub async fn act(sender: OwnedUserId, text: String, room: MatrixRoom) -> Result<
         )> = SystemState::new(&mut world);
 
         let (commands, mut game_info_container_query) = state.get_mut(&mut world);
-        let mut game_info_container = game_info_container_query.single_mut();
+        let game_info_container = game_info_container_query.single_mut();
          
         game_info_container.game_info.clone()
     };
@@ -88,17 +110,45 @@ pub async fn act(sender: OwnedUserId, text: String, room: MatrixRoom) -> Result<
                 if verbose {
                     room.send(RoomMessageEventContent::notice_plain(json_diff)).await.unwrap();
                 }
+
+
+                // update the game state
+                {
+                     // get the GameInfoContainer component from the world
+                    let world_guard = GLOBAL_WORLD.lock().unwrap(); // Error cause by this line.
+                    let mut world = world_guard;
+
+                    // https://doc.qu1x.dev/bevy_trackball/bevy/ecs/system/struct.SystemState.html
+                    // https://github.com/bevyengine/bevy/issues/2687
+                    let mut state: SystemState<(
+                        Commands,
+                        Query<&mut GameInfoContainer>,
+                    )> = SystemState::new(&mut world);
+
+                    let (commands, mut game_info_container_query) = state.get_mut(&mut world);
+
+                    let mut game_info_container = game_info_container_query.single_mut();
+                    game_info_container.game_info = game_info;
+                }
+
+                // form a prompt to describe the users action as a story
+                let act_story_prompt = ACT_STORY_RAW_PROMPT
+                    .replace("${previous_game_state}", &json)
+                    .replace("${new_game_state}", &json_strs[0])
+                    .replace("${action}", &action_prompt);
+
+                if let Ok(result) = get_ai_chat().execute(&None, act_story_prompt.to_string(), Vec::new()) {
+                    info!( "ACT_STORY: {}", result);
+                    room.send(RoomMessageEventContent::notice_plain(result)).await.unwrap();
+                } else {
+                    room.send(RoomMessageEventContent::notice_plain(action_prompt)).await.unwrap();
+                }
             },
             Err(err) => {
                 error!("Error parsing json: {err}");
                 room.send(RoomMessageEventContent::notice_plain("Failed to parse the map info.")).await.unwrap();
             }
         };
-
-
-        // todo: update the game state
-
-        // todo: form a prompt to describe the users action as a story
     }
     
     Ok(())
