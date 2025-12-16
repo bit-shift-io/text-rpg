412ebeacc6f152767a242d77e38babf150e9b07a
Fabian Mathews <fabian@jpmedia.com.au>
2025-12-17 08:53:10 +1030

    Add markdown formatting support.

--- src/services/command_context.rs
+++ src/services/command_context.rs
@@ -103,7 +103,7 @@ impl CommandContext {
     }
 
     pub async fn room_send(&self, msg: &str) -> Result<String, ()> {
-        match self.room.send(RoomMessageEventContent::notice_plain(msg)).await {
+        match self.room.send(RoomMessageEventContent::notice_markdown(msg)).await {
             Ok(response) => Ok("".to_string()),
             Err(e) => {
                 error!("Error sending message: {}", e);
